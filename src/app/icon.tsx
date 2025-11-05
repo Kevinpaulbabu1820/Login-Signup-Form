0d9a5ecdf7a74dbc77dabbf62b27e841813d689c
Kevinpaulbabu1820 <kevinpaulbabu1820@gmail.com>
2025-11-05 23:08:14 +0530

    update

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,3 +1,6 @@
-export default function icon() {
-  return null;
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  // Return a 404 if you don't have an actual icon to serve
+  return new NextResponse(null, { status: 404 });
 }
