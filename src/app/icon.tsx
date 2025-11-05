3124162ca034f35ab66903e512dbc4ca8f66a365
Kevinpaulbabu1820 <kevinpaulbabu1820@gmail.com>
2025-11-05 23:10:53 +0530

    update

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+const handler = async () => {
   // Return a 404 if you don't have an actual icon to serve
   return new NextResponse(null, { status: 404 });
-}
+};
+
+export { handler as GET };
+export default handler;
